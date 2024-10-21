dcde44bf14b58a16d583cf491c80717a5ee6a3ab
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2024-10-21 09:31:07 -0500

    feat: add principal layout

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,37 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import Providers from '../providers';
+import type { Metadata } from 'next';
+import '@/app/globals.css';
+import { Sora } from 'next/font/google';
+import NavBar from './components/ui/nav-bar';
+
+const sora = Sora({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Sijita - Portafolio',
+  description: 'Portafolio de Simón Jiménez Tamayo',
+};
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={sora.className}>
+        <NextIntlClientProvider messages={messages}>
+          <Providers>
+            <NavBar />
+            {children}
+          </Providers>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
